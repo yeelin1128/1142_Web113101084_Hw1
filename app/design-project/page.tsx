7a471fe9b7de8d8d37e00cb01744a26c93e3886d
yeelin1128 <tingyiyyqx@gmail.com>
2026-04-13 03:04:53 +0800

    feat: 04

--- app/design-project/page.tsx
+++ app/design-project/page.tsx
@@ -45,7 +45,9 @@ export default function Hobby() {
               {/* 遮罩：滑鼠移入時變暗 */}
               <div className="absolute inset-0 bg-black/10 group-hover:bg-black/30 transition-colors duration-500" />
               
-              <div className="absolute bottom-0 w-full p-6 text-white backdrop-blur-md bg-white/20 border-t border-white/30">
+              <div className="absolute bottom-0 w-full p-6 text-white backdrop-blur-md bg-white/20 border-t border-white/30
+                              transition-transform duration-500 ease-in-out
+                              translate-y-[calc(100%-80px)] group-hover:translate-y-0">
                 <h3 className="text-xl font-bold mb-1 tracking-wider text-white drop-shadow-lg">
                   {projectData["1"]["name"]}</h3>
                 <p className="text-xs text-white/90 opacity-0 group-hover:opacity-100 translate-y-4 group-hover:translate-y-0 transition-all duration-500 ">
