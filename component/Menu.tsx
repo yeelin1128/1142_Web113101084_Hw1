e31699bd1285741c7412e62cc8d30f89d95240fa
yeelin1128 <tingyiyyqx@gmail.com>
2026-04-12 21:06:43 +0800

    feat: 06

--- component/Menu.tsx
+++ component/Menu.tsx
@@ -8,8 +8,8 @@ export default function Menu() {
   
   return (
     <>
-      <div className="fixed inset-y-0 left-0 
-                      bg-[#acbac4] w-[320px] h-screen p-4 shadow-lg flex flex-col overflow-hidden">
+      <div className="inset-y-0 left-0 
+                      bg-[#acbac4] w-full h-screen p-4 shadow-lg flex flex-col overflow-hidden">
 
         <Link href="/">  
             HOME
